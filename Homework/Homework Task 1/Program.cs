9ff5201956fce5f3c092d5c801037f2142915ab8
VaskoPav <79167640+VaskoPav@users.noreply.github.com>
2021-04-02 20:05:10 +0200

    Add files via upload

---
+++ Homework/Homework Task 1/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Homework_Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            int[] userInputNumbers = new int[6];
+            int sumEvenNumbers = 0;
+
+            for (int i = 0; i < userInputNumbers.Length; i++)
+            {
+                Console.WriteLine("Enter number: ");
+                userInputNumbers[i] = int.Parse(Console.ReadLine());
+                if (userInputNumbers[i] % 2 == 0)
+                {
+                    sumEvenNumbers += userInputNumbers[i];
+
+                }
+                else
+                {
+                    Console.WriteLine("This is odd number: " + userInputNumbers[i]);
+                }
+                
+            }
+            Console.WriteLine("The sum of even number is: " + sumEvenNumbers);
+
+            // 4,3,7,3,2,8
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Homework/Task 2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+
+            string[] studentG1 = new string[]
+            {
+                "Pero",
+                "Pero",
+                "Pero",
+                "Pero",
+                "Pero"
+            };
+            string[] studentG2 = new string[]
+            {
+                "Blazo",
+                "Blazo",
+                "Blazo",
+                "Blazo",
+                "Blazo"
+            };
+
+            do
+            {
+                Console.WriteLine("Enter 1 or 2 to see group of students");
+                switch (Console.ReadLine())
+                {
+                    case "1":
+                        Console.WriteLine("Students in G1 are:");
+                        foreach (string item in studentG1)
+                        {
+                            Console.WriteLine(item);
+                        }
+                        break;
+                    case "2":
+                        Console.WriteLine("Students in G2 are");
+                        foreach (string item2 in studentG2)
+                        {
+                            Console.WriteLine(item2);
+                        }
+                        break;
+
+                    default:
+                        Console.WriteLine("Invalid input");
+                        break;
+                }
+
+
+
+
+            } while (false);
+
+            Console.ReadLine();
+        }
+    }
+}
